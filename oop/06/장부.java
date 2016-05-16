afc35361d12d3317e32d9849fdcf4e7018981904
replaykim <rlawogus1>
2016-05-16 20:10:51 +0900

    06

---
+++ oop/06/장부.java
@@ -0,0 +1,59 @@
+package com.company;
+
+/**
+ * Created by replay on 2016. 5. 10..
+ */
+public class 장부 {
+    private 내역[] 장부;
+    private 내역 최대_결과_내역;
+    private 내역 최소_결과_내역;
+    private int 번호;
+
+    public 장부() {
+        장부  = new 내역[10];
+    }
+
+    public void 내역_등록(String 날짜, String 제목 , int 금액, String 비고) {
+            장부[번호] = new 내역(날짜,제목,금액,비고);
+            번호 ++;
+        for (int i = 번호;i<장부.length;i++){
+            장부[i] = new 내역("","",0,"");
+        }
+    }
+
+    public void 최대금액_얻기() {
+        int 임시, 최대금액;
+        최대_결과_내역 = 장부[0];
+        최대금액 = 장부[0].get금액();
+        for (int i = 0 ; i < 장부.length; i++){
+            임시 = 장부[i].get금액();
+            if (최대금액 < 임시){
+                최대_결과_내역 = 장부[i];
+            }
+        }
+    }
+
+    public void 최소금액_얻기(){
+        int 임시, 최소금액;
+        최소_결과_내역 = 장부[0];
+        최소금액 = 장부[0].get금액();
+        for (int i = 0 ; i < 장부.length; i++){
+            임시 = 장부[i].get금액();
+            if (최소금액 > 임시){
+                최소_결과_내역 = 장부[i];
+            }
+        }
+    }
+
+    public void set장부(내역[] 장부) {
+        this.장부 = 장부;
+    }
+
+    public 내역 get최소_결과_내역() {
+        return 최소_결과_내역;
+    }
+
+    public 내역 get최대_결과_내역() {
+        return 최대_결과_내역;
+    }
+}
---
+++ oop/06/제어자.java
@@ -0,0 +1,61 @@
+package com.company;
+
+public class 제어자 {
+    private 장부 장부;
+    private 입력 입력;
+    private 출력 출력;
+
+
+    public 제어자() {
+        입력 = new 입력();
+        출력 = new 출력();
+        장부 = new 장부();
+    }
+
+    public void 제어() {
+        boolean 끝;
+        String 날짜;
+        String 제목;
+        int  금액;
+        String 비고;
+        내역 최대, 최소;
+        do {
+            끝 = 입력.내역_입력();
+            날짜 = 입력.get날짜();
+            제목 = 입력.get제목();
+            금액 = 입력.get금액();
+            비고 = 입력.get비고();
+            장부.내역_등록(날짜,제목,금액,비고);
+        }while (끝);
+
+        장부.최대금액_얻기();
+        최대 = 장부.get최대_결과_내역();
+        날짜 = 최대.get날짜();
+        제목 = 최대.get제목();
+        금액 = 최대.get금액();
+        비고 = 최대.get비고();
+        출력.set날짜(날짜);
+        출력.set제목(제목);
+        출력.set금액(금액);
+        출력.set비고(비고);
+        출력.출력();
+        장부.최소금액_얻기();
+
+        최소 = 장부.get최소_결과_내역();
+        날짜 = 최소.get날짜();
+        제목 = 최소.get제목();
+        금액 = 최소.get금액();
+        비고 = 최소.get비고();
+        출력.set날짜(날짜);
+        출력.set제목(제목);
+        출력.set금액(금액);
+        출력.set비고(비고);
+        출력.출력();
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+        제어자 제어자 = new 제어자();
+        제어자.제어();
+    }
+}
